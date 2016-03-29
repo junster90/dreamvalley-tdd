e80d445b162dcd398f0e489cf776cc4aa8119d57
Mindvalley <Mindvalley@Chai-Wei-Jun-MBP.local>
2016-03-29 15:50:07 +0800

    fixed spec mistakes

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,18 @@ class User < ActiveRecord::Base
   def self.authorised?(user)
     user.email.ends_with?("@mindvalley.com" || "@mindvalleysupport.com")
   end
+
+  def self.from_omniauth(auth)
+
+  end
+
+  def assign_from_omniauth(auth)
+    self.provider = auth["provider"]
+    self.uid = auth["uid"]
+    self.first_name = auth["info"]["first_name"]
+    self.last_name = auth['info']['last_name']
+    self.email = auth["info"]["email"]
+    self.token = auth["credentials"]["token"]
+    self.id_token = auth["credentials"]["id_token"]
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,19 +11,20 @@ describe User do
     end
 
     it "can create an account with omniauth" do 
-      expect(User.from_omniauth(auth_hash)).to change(User.count).by(1)
+      expect{User.from_omniauth(auth_hash)}.to change{User.count}.by(1)
     end
   end
 
   describe "non mindvalley employees" do
+    auth_hash = FactoryGirl.create(:mindvalley_auth_hash, :customer)
+
     it "should not be authorised" do
-      auth_hash = FactoryGirl.create(:mindvalley_auth_hash, :customer)
       user = User.new.assign_from_omniauth(auth_hash)
       expect(User.authorised?(user)).to_not be_truthy
     end
 
     it "cannot create an account with omniauth" do
-      expect(User.from_omniauth(auth_hash)).to_not change(User.count)
+      expect{User.from_omniauth(auth_hash)}.to_not change{User.count}
     end
   end
 end
