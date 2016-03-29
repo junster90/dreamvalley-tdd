d4395be0e86a324a13962f1a58c5e176a035b7f5
Mindvalley <Mindvalley@Chai-Wei-Jun-MBP.local>
2016-03-29 18:35:56 +0800

    using enumerize to organize goal type

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -2,4 +2,7 @@ class Goal < ActiveRecord::Base
   belongs_to :user
 
   validates :type, :description, :user, presence: true
+
+  extend Enumerize
+  enumerize :type, in: [:experience, :growth, :contribution], predicates:true
 end
