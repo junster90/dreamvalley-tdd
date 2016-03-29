0f671b87a5b8a07858584c849e36ac469f7980c9
Mindvalley <Mindvalley@Chai-Wei-Jun-MBP.local>
2016-03-29 15:12:12 +0800

    created factory for omniauth hash

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,17 +1,17 @@
-FactoryGirl.define do 
-  factory :employee, class: User do
-    first_name "Vishen"
-    last_name "Lakhiani"
-    email "vishen@mindvalley.com"
-    provider "mindvalley"
-    uid "1"
-  end
+# FactoryGirl.define do 
+#   factory :employee, class: User do
+#     first_name "Vishen"
+#     last_name "Lakhiani"
+#     email "vishen@mindvalley.com"
+#     provider "mindvalley"
+#     uid "1"
+#   end
 
-  factory :customer, class: User do
-    first_name "Najib"
-    last_name "Razak"
-    email "najib@ambank.com.my"
-    provider "mindvalley"
-    uid "2"
-  end
-end
+#   factory :customer, class: User do
+#     first_name "Najib"
+#     last_name "Razak"
+#     email "najib@ambank.com.my"
+#     provider "mindvalley"
+#     uid "2"
+#   end
+# end
