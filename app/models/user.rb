9e65057511826d54a0506cf39b5bb50316f484c5
Mindvalley <Mindvalley@Chai-Wei-Jun-MBP.local>
2016-03-29 16:26:21 +0800

    edited tests to account for omniauth only authentication, written user methods to pass test

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,20 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:mindvalley]
 
   def self.authorised?(user)
     user.email.ends_with?("@mindvalley.com" || "@mindvalleysupport.com")
   end
 
   def self.from_omniauth(auth)
+    user = where(provider: auth["provider"], uid: auth["uid"].to_s).first || where(email: auth["info"]["email"]).first || self.new()
+    user.assign_from_omniauth(auth)
+    if User.authorised?(user)
+      return user
+    else
+      return nil
+    end
 
   end
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,12 +6,13 @@ describe User do
     auth_hash = FactoryGirl.create(:mindvalley_auth_hash, :employee)
 
     it "should be authorised" do
-      user = User.new.assign_from_omniauth(auth_hash)
+      user = User.new
+      user.assign_from_omniauth(auth_hash)
       expect(User.authorised?(user)).to be_truthy
     end
 
-    it "can create an account with omniauth" do 
-      expect{User.from_omniauth(auth_hash)}.to change{User.count}.by(1)
+    it "will persist to the controller" do 
+      expect(User.from_omniauth(auth_hash)).to_not be_nil
     end
   end
 
@@ -19,12 +20,13 @@ describe User do
     auth_hash = FactoryGirl.create(:mindvalley_auth_hash, :customer)
 
     it "should not be authorised" do
-      user = User.new.assign_from_omniauth(auth_hash)
+      user = User.new
+      user.assign_from_omniauth(auth_hash)
       expect(User.authorised?(user)).to_not be_truthy
     end
 
-    it "cannot create an account with omniauth" do
-      expect{User.from_omniauth(auth_hash)}.to_not change{User.count}
+    it "will not persist to the controller" do
+      expect(User.from_omniauth(auth_hash)).to be_nil
     end
   end
 end
