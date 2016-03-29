2cc249f0d23ead15fae3ec832888af9dd81ad28e
Mindvalley <Mindvalley@Chai-Wei-Jun-MBP.local>
2016-03-29 15:41:38 +0800

    deleted commented code

--- spec/factories/user.rb
+++
@@ -1,9 +0,0 @@
-# FactoryGirl.define do 
-#   factory :employee, class: User do
-#     first_name "Vishen"
-#     last_name "Lakhiani"
-#     email "vishen@mindvalley.com"
-#     provider "mindvalley"
-#     uid "1"
-#   end
-# end
