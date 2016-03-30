92258720c413f4f16d383cc29d82772d3063ff96
Mindvalley <Mindvalley@Chai-Wei-Jun-MBP.local>
2016-03-30 17:42:37 +0800

    added devise login helper to test authentication

--- spec/controllers/goals_controller_spec.rb
+++ spec/controllers/goals_controller_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe GoalsController, type: :controller do
+  describe '#new' do
+    context "user not logged in" do
+      it "sends user to login" do
+        sign_in(nil)
+        get :new
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
 
+    context "user is logged in" do
+      it "allows user to access creation page" do
+        sign_in
+        get :new
+        expect(response).to be_success
+      end
+    end
+  end
 end
