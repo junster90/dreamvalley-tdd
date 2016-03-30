eba61efd0be73cd7efbd6007f5389c5b019cd436
Mindvalley <Mindvalley@Chai-Wei-Jun-MBP.local>
2016-03-30 16:42:25 +0800

    passed test

--- spec/features/user_visits_homepage_spec.rb
+++ spec/features/user_visits_homepage_spec.rb
@@ -2,7 +2,6 @@ require "rails_helper"
 
 RSpec.feature "User visits homepage" do
   scenario "they can see random goals" do
-    byebug
     goal = FactoryGirl.create(:goal)
     visit root_path
     expect(page).to have_text goal.description
