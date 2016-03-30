0e025a1c20e2a6587d8ff31f497802ca687466c3
Mindvalley <Mindvalley@Chai-Wei-Jun-MBP.local>
2016-03-30 14:00:44 +0800

    written spec for home controller index

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe HomeController, type: :controller do
+  subject { get :index }
+  describe '#index' do
+    it "assigns random goals to instance" do
+      5.times {FactoryGirl.build(:goal)}
+      subject
+      expect{assigns(:random_goals)}.to eq Goal.all
+    end
 
+    it "renders the index page" do
+      expect(subject).to render_template(:index)
+    end
+  end
 end
--- spec/factories/goals.rb
+++ spec/factories/goals.rb
@@ -1,19 +1,31 @@
 FactoryGirl.define do
   factory :goal do
-    type :contribution
-    description "MyText"
-    user User.new(id: 1, email: "example@example.com")
+    sequence :description do |n|
+      "I want to sample description #{n}"
+    end
+
+    after(:build) do |g|
+      random_user_id = (1..5).to_a.sample
+      g.type = Goal.type.values.sample
+      g.user = User.find_or_create_by(id: random_user_id, email: "user#{random_user_id}@example.com")
+    end
 
     trait :no_type do
-      type nil
+      after(:build) do |g|
+        g.type = nil
+      end
     end
 
     trait :no_description do
-      description nil
+      after(:build) do |g|
+        g.description = nil
+      end
     end
 
     trait :no_user do
-      user nil
+      after(:build) do |g|
+        g.user = nil
+      end
     end
   end
 end
