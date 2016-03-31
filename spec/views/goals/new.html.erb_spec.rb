bf4e2742d4369c9c235cdcd3f27433ebb83c21d2
Mindvalley <Mindvalley@Chai-Wei-Jun-MBP.local>
2016-03-31 15:35:31 +0800

    corrected specs and passed view tests

--- spec/views/goals/new.html.erb_spec.rb
+++ spec/views/goals/new.html.erb_spec.rb
@@ -2,12 +2,16 @@ require "rails_helper"
 
 RSpec.describe "goals/new.html.erb" do
   describe "the form is properly displayed" do
+    before do
+      render
+    end
+
     it "has a category selector" do
-      expect(rendered).to have_selector('goal[category]')
+      expect(rendered).to have_selector(:id, 'goal_category')
     end
 
     it 'has a description field' do
-      expect(rendered).to have_selector('goal[description]')
+      expect(rendered).to have_selector(:id, 'goal_description')
     end
 
     it 'has a submit button' do
