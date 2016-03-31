c3d9dcfa31c5e35cb6ad3561df81defedee6f7ab
Mindvalley <Mindvalley@Chai-Wei-Jun-MBP.local>
2016-03-31 12:23:34 +0800

    written specs

--- spec/features/user_creates_goal_spec.rb
+++ spec/features/user_creates_goal_spec.rb
@@ -2,5 +2,15 @@ require "rails_helper"
 Warden.test_mode!
 
 RSpec.feature "User creates goal" do
+  before do
+    user = FactoryGirl.create(:user)
+    login_as(user)
+  end
+
+  scenario "they see the goal creation form" do
+    visit new_goal_path
+    Capybara.default_selector = :css
+    expect(page).to have_selector("input")
+  end
 
 end
