d5792384a54f8c0d2535b439fc38dac1c87ebceb
Mindvalley <Mindvalley@Chai-Wei-Jun-MBP.local>
2016-03-31 11:29:23 +0800

    added login features needed to create goals

--- spec/features/user_visits_homepage_spec.rb
+++ spec/features/user_visits_homepage_spec.rb
@@ -11,4 +11,16 @@ RSpec.feature "User visits homepage" do
     visit root_path
     expect(page).to have_link "Write your own today!", href:new_goal_path
   end
+
+  scenario "they can login" do
+    sign_in nil
+    visit root_path
+    expect(page).to have_link "Login", href: login_path
+  end
+
+  scenario "see their name when they're logged in" do
+    sign_in
+    visit root_path
+    expect(page).to have_text "#{controller.current_user.first_name}"
+  end
 end
